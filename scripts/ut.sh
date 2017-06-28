7eacb2588d7534abbbd46d63aec577bedf3b0dd3
Mohammad Asif Siddiqui <mohammad.asif.siddiqui1@huawei.com>
2017-06-28 14:49:51 +0800

    Updated the UT scripts with go test tool
    Fix to generate the coverage files

--- scripts/ut.sh
+++ scripts/ut.sh
@@ -6,9 +6,9 @@ for d in $(go list ./... | grep -v vendor); do
     cd $HOME/gopath/src/
     cd $d
     set +e
-    ginkgo -r -v -cover  
-    if [ -f *.coverprofile ]; then
-        sed '1d;$d' *.coverprofile >> $HOME/gopath/src/github.com/servicecomb/service-center/coverage.txt
+    go test -cover -covermode atomic -coverprofile coverage.out  
+    if [ -f coverage.out ]; then
+        sed '1d;$d' coverage.out >> $HOME/gopath/src/github.com/servicecomb/service-center/coverage.txt
     fi
     set -e
 done
