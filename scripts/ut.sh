84a74fad181db91e5619a09c4b9061f2ba454034
Mohammad Asif Siddiqui <mohammad.asif.siddiqui1@huawei.com>
2017-06-29 21:15:36 +0800

    Update the UT scripts to fail in case UT failure

--- scripts/ut.sh
+++ scripts/ut.sh
@@ -3,12 +3,11 @@ set -e
 export COVERAGE_PATH=$(pwd)
 cd $1
 for d in $(go list ./... | grep -v vendor); do
-    cd $HOME/gopath/src/
-    cd $d
-    set +e
-    go test -cover -covermode atomic -coverprofile coverage.out  
-    if [ -f coverage.out ]; then
-        sed '1d;$d' coverage.out >> $HOME/gopath/src/github.com/servicecomb/service-center/coverage.txt
+    cd $HOME/gopath/src/$d
+    if [ $(ls | grep _test.go | wc -l) -gt 0 ]; then
+        go test -cover -covermode atomic -coverprofile coverage.out  
+        if [ -f coverage.out ]; then
+            sed '1d;$d' coverage.out >> $HOME/gopath/src/github.com/servicecomb/service-center/coverage.txt
+        fi
     fi
-    set -e
 done
