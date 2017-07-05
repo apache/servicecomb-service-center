5cfc5e43f75e10b3c6686dc8ee89a4ad97cd111c
asifdxtreme <mohammad.asif.siddiqui1@huawei.com>
2017-07-05 09:08:17 +0800

    Updated the UT scripts to locally run etcd in docker

--- tests/scripts/ut_test_in_docker.sh
+++ tests/scripts/ut_test_in_docker.sh
@@ -1,82 +1,38 @@
 #!/bin/bash
 
-# //////////////////////////////////////////////////////////////////////////// #
-#                                                                              #
-#                              Global Variables                                #
-#                                                                              #
-# //////////////////////////////////////////////////////////////////////////// #
 set -x
 set +e
-export c_name="etcd_sc"
+export c_name="etcd"
 docker rm -f $c_name
 set -e
-export BUILD_DIR=$(cd $(dirname $0); pwd)
-export PROJECT_DIR=$(cd $BUILD_DIR/../../;pwd)
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+echo "${green}Starting Unit Testing for Service Center${reset}"
 
-# //////////////////////////////////////////////////////////////////////////// #
-#                                                                              #
-#                              Functions                                       #
-#                                                                              #
-# //////////////////////////////////////////////////////////////////////////// #
-
-# //////////////////////////////////////////////////////////////////////////// #
-#                                                                              #
-#                              Executions                                      #
-#                                                                              #
-# //////////////////////////////////////////////////////////////////////////// #
-echo "ut test hsa-service-center"
-bash $PROJECT_DIR/tests/scripts/setup_dependency.sh
-export GIT_SSL_NO_VERIFY=1
-git config --global http.sslverify false
-go install github.com/onsi/ginkgo/ginkgo
-go install github.com/onsi/gomega
-
-
-docker run -d -v /usr/share/ca-certificates/:/etc/ssl/certs -p 40010:40010 -p 23800:23800 -p 23790:23790 \
-  --name $c_name 10.162.197.95:5000/cse/etcd:v3.1.0 \
- etcd -name etcd0 \
- -advertise-client-urls http://${HostIP}:23790,http://${HostIP}:40010 \
- -listen-client-urls http://0.0.0.0:23790,http://0.0.0.0:40010 \
- -initial-advertise-peer-urls http://${HostIP}:23800 \
- -listen-peer-urls http://0.0.0.0:23800 \
- -initial-cluster-token etcd-cluster-1 \
- -initial-cluster etcd0=http://${HostIP}:23800 \
- -initial-cluster-state new
-while ! nc -z 127.0.0.1 23790; do
-  echo "Waiting Etcd to launch on 23790..."
+echo "${green}Starting etcd in docker${reset}"
+docker run -d -v /usr/share/ca-certificates/:/etc/ssl/certs -p 40010:40010 -p 23800:23800 -p 2379:2379 --name etcd quay.io/coreos/etcd etcd -name etcd0 -advertise-client-urls http://127.0.0.1:2379,http://127.0.0.1:40010 -listen-client-urls http://0.0.0.0:2379,http://0.0.0.0:40010 -initial-advertise-peer-urls http://127.0.0.1:23800 -listen-peer-urls http://0.0.0.0:23800 -initial-cluster-token etcd-cluster-1 -initial-cluster etcd0=http://127.0.0.1:23800 -initial-cluster-state new
+while ! nc -z 127.0.0.1 2379; do
+  echo "Waiting Etcd to launch on 2379..."
   sleep 1
 done
-ut_root="$PROJECT_DIR/src/github.com/servicecomb/service-center/server/service"
-export GOPATH=${PROJECT_DIR}
+echo "${green}Etcd is running......${reset}"
 
-if [ ! -d "${ut_root}/conf/" ]; then
-    mkdir -p ${ut_root}/conf/
-fi
+echo "${green}Preparing the env for UT....${reset}"
+./scripts/prepare_env_ut.sh
 
-export APP_ROOT=${ut_root}
-export SSL_ROOT=$PROJECT_DIR/deployment/release/cse-service-center/etc/ssl
-export CIPHER_ROOT=$PROJECT_DIR/deployment/release/cse-service-center/etc/cipher
-export registry_address="127.0.0.1:23790"
-ssl_mode="${CSE_SSL_MODE:-"0"}"
-#sed -i "s/\${ssl_passphase}/00000001000000014DCF0FE2FEDEE0D92A0018D45F5A8631D45C4905A7420F6111A2947E05A4FBBD/g" $PROJECT_DIR/deployment/release/cse-service-center/etc/ssl/ssl.properties
-
-cp $PROJECT_DIR/deployment/release/cse-service-center/conf/* ${ut_root}/conf/
-sed -i s/"ssl_mode.*=.*$"/"ssl_mode = $ssl_mode"/g ${ut_root}/conf/app.conf
-sed -i s@"manager_cluster.*=.*$"@"manager_cluster = \"$registry_address\""@g ${ut_root}/conf/app.conf
-
-cp -r ${ut_root}/conf ${ut_root}/microservice/
-
-ginkgo -r -v -cover ${ut_root}
+echo "${green}Running UT for Service-Center server"
+bash -x ./scripts/ut.sh server
+  
 if [ $? == 0 ]; then
-	echo "test service-center ok"
+	echo "${green}All the unit test passed..${reset}"
+	echo "${green}Coverage is created in the file ./coverage.txt${reset}"
 else
-	echo "test service-center failed"
-
+	echo "${red}Some or all the unit test failed..please check the logs for more details.${reset}"
 	exit 1
 fi
 
-echo "finished build service-center"
+echo "${green}Service-Center finished${reset}"
 
+echo "${green}Cleaning up the etcd docker container${reset}"
 docker rm -f $c_name
-
-
