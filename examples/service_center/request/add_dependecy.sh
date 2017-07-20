f5a16d18c60c9bbc32f807a1ed579a45af850b04
Mohammad Asif Siddiqui <mohammad.asif.siddiqui1@huawei.com>
2017-07-20 10:50:09 +0800

    Update add_dependecy.sh

--- examples/service_center/request/add_dependecy.sh
+++ examples/service_center/request/add_dependecy.sh
@@ -1,8 +1,26 @@
 #!/usr/bin/env bash
-curl -X POST -H "Content-Type: application/json" -d '[{
-    "service_uuid":"0d7eae6e-aa81-4fcc-aeea-09f83a46e3d8",
-    "depend_on":"1bd77a5b-e89a-4361-8ba1-0254bcb012f0"
-},{
-    "service_uuid":"0d7eae6e-aa81-4fcc-aeea-09f83a46e3d8",
-    "depend_on":"519d7463-d4b4-4d11-b54d-8f66786970d9"
-}]' "http://127.0.0.1:9980/service_center/v2/dependency/0d7eae6e-aa81-4fcc-aeea-09f83a46e3d8"
+PUT /registry/v3/dependencies HTTP/1.1
+Host: localhost:30100
+Content-Type: application/json
+x-domain-name: default
+Cache-Control: no-cache
+Postman-Token: e924c839-df99-5526-5c30-f7ca9a671316
+
+{
+    "dependencies": [
+        {
+            "consumer": {
+                "appId": "TestApp8",
+                "serviceName": "Test8",
+                "version": "1.0.0"
+            },
+            "providers": [
+                {
+                     "serviceName": "*",
+                     "appId": "TestApp3",
+                     "version":"1.0.0"
+                }
+            ]
+        }
+    ]
+}
