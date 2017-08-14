cfba4e8c5fba3fcf6d7d2748e9fbfc390e97afff
little-cui <sure_0757@qq.com>
2017-08-14 19:15:41 +0800

    Set the http client default timeout. (#64)

--- util/rest/client.go
+++ util/rest/client.go
@@ -54,7 +54,7 @@ func getTLSTransport(verifyPeer bool, supplyCert bool, verifyCN bool) (transport
 */
 var httpClient = &http.Client{
 	Transport: transport,
-	Timeout:   30 * time.Second,
+	Timeout:   300 * time.Second,
 }
 
 var maxIdleConnsPerHost = beego.AppConfig.DefaultInt("max_idle_conns_per_host", 200)
@@ -91,7 +91,7 @@ func getHttpsClient(gzip, verifyPeer, supplyCert, verifyCN bool) (client *HttpCl
 		gzip: gzip,
 		client: &http.Client{
 			Transport: transport,
-			Timeout:   30 * time.Second,
+			Timeout:   300 * time.Second,
 		},
 	}
 
