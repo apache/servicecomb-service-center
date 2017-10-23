6b5bf139655d350bbf3af0be587631e94a8f5a68
little-cui <sure_0757@qq.com>
2017-10-23 20:23:26 +0800

    Optimize request context.

--- server/rest/controller/v4/v4.go
+++ server/rest/controller/v4/v4.go
@@ -56,34 +56,32 @@ func (v *v4Context) IsMatch(r *http.Request) bool {
 }
 
 func (v *v4Context) Do(r *http.Request) error {
-	path, err := url.PathUnescape(r.RequestURI)
-	if err != nil {
-		util.Logger().Errorf(err, "Invalid Request URI %s", r.RequestURI)
-		return err
+	ctx := r.Context()
+	if ctx.Value("tenant") == nil {
+		path, err := url.PathUnescape(r.RequestURI)
+		if err != nil {
+			util.Logger().Errorf(err, "Invalid Request URI %s", r.RequestURI)
+			return err
+		}
+
+		start := len("/v4/")
+		end := start + strings.Index(path[start:], "/")
+
+		tenant := strings.TrimSpace(path[start:end])
+		if len(tenant) == 0 {
+			err := errors.New("Header does not contain domain.")
+			util.Logger().Errorf(err, "Invalid Request URI %s", r.RequestURI)
+			return err
+		}
+		util.WithContext(r, "tenant", tenant)
 	}
 
-	start := len("/v4/")
-	end := start + strings.Index(path[start:], "/")
-
-	tenant := strings.TrimSpace(path[start:end])
-	if len(tenant) == 0 {
-		err := errors.New("Header does not contain domain.")
-		util.Logger().Errorf(err, "Invalid Request URI %s", r.RequestURI)
-		return err
-	}
-
-	project := r.Header.Get("X-Domain-Name")
-	if len(project) == 0 {
-		project = r.Header.Get("X-Tenant-Name")
+	if ctx.Value("project") == nil {
+		project := r.Header.Get("X-Domain-Name")
 		if len(project) == 0 {
 			project = core.REGISTRY_PROJECT
 		}
+		util.WithContext(r, "project", project)
 	}
-
-	ctx := r.Context()
-	ctx = util.NewContext(ctx, "tenant", tenant)
-	ctx = util.NewContext(ctx, "project", project)
-	request := r.WithContext(ctx)
-	*r = *request
 	return nil
 }
