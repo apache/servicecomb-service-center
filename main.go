2a6000391543369187c0e1af3c78e1720e0444ae
Mohammad Asif Siddiqui <mohammad.asif.siddiqui1@huawei.com>
2017-12-17 09:15:14 +0800

    Updating License Header for all files (#208)
    * Updating License Header
    
    * Updating License Header for *.js files
    
    * Updating License Header for *.html files
    
    (cherry picked from commit f17fc25)

--- main.go
+++ main.go
@@ -1,21 +1,19 @@
-//Copyright 2017 Huawei Technologies Co., Ltd
-//
-//Licensed under the Apache License, Version 2.0 (the "License");
-//you may not use this file except in compliance with the License.
-//You may obtain a copy of the License at
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-//Unless required by applicable law or agreed to in writing, software
-//distributed under the License is distributed on an "AS IS" BASIS,
-//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//See the License for the specific language governing permissions and
-//limitations under the License.
-// @APIVersion 1.0.0
-// @APITitle Service manager API
-// @APIDescription Service manager API
-// @BasePath http://127.0.0.1:8080/
-// @Contact tianxiaoliang3@huawei.com
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package main
 
 // plugins
--- pkg/util/util.go
+++ pkg/util/util.go
@@ -1,16 +1,19 @@
-//Copyright 2017 Huawei Technologies Co., Ltd
-//
-//Licensed under the Apache License, Version 2.0 (the "License");
-//you may not use this file except in compliance with the License.
-//You may obtain a copy of the License at
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-//Unless required by applicable law or agreed to in writing, software
-//distributed under the License is distributed on an "AS IS" BASIS,
-//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//See the License for the specific language governing permissions and
-//limitations under the License.
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package util
 
 import (
@@ -49,9 +52,9 @@ func ClearStringMemory(src *string) {
 		len int
 	})(unsafe.Pointer(src))
 
-	len := MinInt(p.len, 32)
+	l := MinInt(p.len, 32)
 	ptr := p.ptr
-	for idx := 0; idx < len; idx = idx + 1 {
+	for idx := 0; idx < l; idx = idx + 1 {
 		b := (*byte)(unsafe.Pointer(ptr))
 		*b = 0
 		ptr += 1
@@ -59,8 +62,8 @@ func ClearStringMemory(src *string) {
 }
 
 func ClearByteMemory(src []byte) {
-	len := MinInt(len(src), 32)
-	for idx := 0; idx < len; idx = idx + 1 {
+	l := MinInt(len(src), 32)
+	for idx := 0; idx < l; idx = idx + 1 {
 		src[idx] = 0
 	}
 }
