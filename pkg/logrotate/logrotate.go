054a3404715f37e585caa245b7d597ba525d8d4c
aseTo2016 <1473442069@qq.com>
2018-01-18 20:09:45 +0800

    [scb255]logrotate bug fix ,non write permission, skip

--- pkg/logrotate/logrotate.go
+++ pkg/logrotate/logrotate.go
@@ -246,12 +246,16 @@ func FilterFileList(path, pat string) ([]string, error) {
 	capacity := 10
 	//initialize a fileName slice, len=0, cap=10
 	fileList := make([]string, 0, capacity)
+	isSkip := func(f os.FileInfo) bool{
+		//dir or non write permission,skip
+		return f.IsDir() || (f.Mode() & 0200 == 0000)
+	}
 	err := filepath.Walk(path,
 		func(pathName string, f os.FileInfo, e error) error {
 			if f == nil {
 				return e
 			}
-			if f.IsDir() {
+			if isSkip(f) {
 				return nil
 			}
 			if pat != "" {
