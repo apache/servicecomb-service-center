1b60827e65107b324ca23b0acfc2b33581fd4b32
Mohammad Asif Siddiqui <mohammad.asif.siddiqui1@huawei.com>
2018-01-29 10:32:09 +0530

    Update make_linux_release.sh

--- scripts/release/make_linux_release.sh
+++ scripts/release/make_linux_release.sh
@@ -13,7 +13,7 @@ cp -r etc/conf servicecomb-service-center-1.0.0-m1-linux-amd64/
 cp -r LICENCE servicecomb-service-center-1.0.0-m1-linux-amd64/
 cp -r NOTICE servicecomb-service-center-1.0.0-m1-linux-amd64/
 cp -r DISCLAIMER servicecomb-service-center-1.0.0-m1-linux-amd64/
-cp -r README servicecomb-service-center-1.0.0-m1-linux-amd64/
+cp -r README.md servicecomb-service-center-1.0.0-m1-linux-amd64/
 
 ## Tar the release
 tar -czvf servicecomb-service-center-1.0.0-m1-linux-amd64.tar.gz servicecomb-service-center-1.0.0-m1-linux-amd64
