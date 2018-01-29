ba6ed4bb8c482f6f16d834d4711b3932fbff9c6d
Mohammad Asif Siddiqui <mohammad.asif.siddiqui1@huawei.com>
2018-01-29 10:31:52 +0530

    Update Linux _Release scripts

--- scripts/release/make_linux_release.sh
+++ scripts/release/make_linux_release.sh
@@ -2,6 +2,11 @@ rm -rf servicecomb-service-center-1.0.0-m1-linux-amd64
 mkdir -p servicecomb-service-center-1.0.0-m1-linux-amd64
 
 ## Make the Binary
+export GIT_COMMIT=$(git log  --pretty=format:'%h' -n 1)
+export BUILD_NUMBER=1.0.0
+GO_LDFLAGS="${GO_LDFLAGS} -X 'github.com/apache/incubator-servicecomb-service-center/version.BUILD_TAG=$(date +%Y%m%d%H%M%S).$BUILD_NUMBER.$GIT_COMMIT'"
+GO_LDFLAGS="${GO_LDFLAGS} -X 'github.com/apache/incubator-servicecomb-service-center/version.VERSION=$BUILD_NUMBER'"
+go build --ldflags "${GO_LDFLAGS}"
 go build -o service-center
 cp -r service-center servicecomb-service-center-1.0.0-m1-linux-amd64
 cp -r etc/conf servicecomb-service-center-1.0.0-m1-linux-amd64/
