d87c01b5fee97ca70f404d842909576f23eede68
asifdxtreme <mohammad.asif.siddiqui1@huawei.com>
2018-03-11 12:15:41 +0530

    Update the release scripts to replace incubator with incubating

--- scripts/release/start_scripts/windows/start-service-center.bat
+++ scripts/release/start_scripts/windows/start-service-center.bat
@@ -13,4 +13,4 @@
 :: See the License for the specific language governing permissions and
 :: limitations under the License.
 
-apache-incubator-servicecomb-service-center.exe
+apache-incubating-servicecomb-service-center.exe
