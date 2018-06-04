310c71a4fd0c109be3d54762b5993daa28e5b518
m00416667 <mengtao4@huawei.com>
2018-06-04 21:21:26 +0800

    Modify connection etcd timeout configurable

--- server/infra/registry/registry.go
+++ server/infra/registry/registry.go
@@ -24,16 +24,18 @@ import (
 	"golang.org/x/net/context"
 	"strconv"
 	"time"
+	"github.com/apache/incubator-servicecomb-service-center/pkg/util"
 )
 
 var defaultRegistryConfig Config
-var err error
 
 func init() {
 	defaultRegistryConfig.ClusterAddresses = beego.AppConfig.DefaultString("manager_cluster", "sc-0=http://127.0.0.1:2380")
 	requestTimeConfig := beego.AppConfig.DefaultString("registry_timeout", "30s")
+	var err error
 	defaultRegistryConfig.RequestTimeOut, err = time.ParseDuration(requestTimeConfig)
 	if err != nil {
+		util.Logger().Errorf(err, "registry_timeout is invaild, use default time 30s")
 	    defaultRegistryConfig.RequestTimeOut, _ = time.ParseDuration("30s")
 	}
 }
