a323891d3d33ebd22cd4a37e6f1747caaae9a0fe
Zen Lin <linzhinan@huawei.com>
2019-05-27 20:35:47 +0800

    correct cmdlilne message
    Signed-off-by: Zen Lin <linzhinan@huawei.com>

--- syncer/cmd/root.go
+++ syncer/cmd/root.go
@@ -8,7 +8,9 @@ import (
 
 var rootCmd = &cobra.Command{
 	Use:   "syncer",
-	Short: "Syncer is a synchronization tool for multiple service centers",
+	Short: "Syncer is a synchronization tool for mutilple service centers",
+	Long: "Syncer is a synchronization tool for mutilple service centers, supported: ServiceComb service-center, " +
+		"going to be supported: SpringCloud Eureka, Istio, K8S",
 }
 
 func Execute() error {
