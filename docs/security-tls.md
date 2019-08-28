1c4f8f30f1a0cef0bf3ae0cba378e7e798757e1e
humingcheng <humingcheng1990@163.com>
2019-08-28 14:11:52 +0800

    Support TLS 1.3 (#582)

--- docs/security-tls.md
+++ docs/security-tls.md
@@ -14,5 +14,5 @@ Please modify the conf/app.conf before start up SC
 
 1. ssl_mode: Enabled SSL/TLS mode. [0, 1]
 1. ssl_verify_client: Whether the SC verify client(including etcd server). [0, 1]
-1. ssl_protocols: Minimal SSL/TLS protocol version. ["TLSv1.0", "TLSv1.1", "TLSv1.2"]
+1. ssl_min_version: Minimal SSL/TLS protocol version. ["TLSv1.0", "TLSv1.1", "TLSv1.2", "TLSv1.3"], based on Go version
 1. ssl_ciphers: A list of cipher suite. By default, uses TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,TLS_RSA_WITH_AES_256_GCM_SHA384,TLS_RSA_WITH_AES_128_GCM_SHA256
--- pkg/tlsutil/common.go
+++ pkg/tlsutil/common.go
@@ -1,19 +1,18 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Licensed to the Apache Software Foundation (ASF) under one or more
+// contributor license agreements.  See the NOTICE file distributed with
+// this work for additional information regarding copyright ownership.
+// The ASF licenses this file to You under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with
+// the License.  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package tlsutil
 
 import (
@@ -35,10 +34,17 @@ var TLS_VERSION_MAP = map[string]uint16{
 	"TLSv1.2": tls.VersionTLS12,
 }
 
-var TLS_CIPHER_SUITE []uint16
+var cipherSuite []uint16
 
-func init() {
-	for _, c := range TLS_CIPHER_SUITE_MAP {
-		TLS_CIPHER_SUITE = append(TLS_CIPHER_SUITE, c)
+// MaxSupportedTLSVersion is the max supported TLS version
+var MaxSupportedTLSVersion uint16 = tls.VersionTLS12
+
+func TLSCipherSuits() []uint16 {
+	if cipherSuite != nil {
+		return cipherSuite
 	}
+	for _, c := range TLS_CIPHER_SUITE_MAP {
+		cipherSuite = append(cipherSuite, c)
+	}
+	return cipherSuite
 }
