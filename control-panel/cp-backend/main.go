de7a5e8a371b3433c1b336e81e6d7c31fd0caa54
alec.zheng <aleczheng05@gmail.com>
2020-08-19 09:36:00 +0800

    add proxy to service-center api

--- control-panel/cp-backend/main.go
+++ control-panel/cp-backend/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/apache/servicecomb-service-center/control-panel/cp-backend/pusher"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"net/url"
 )
 
 func main() {
@@ -16,14 +17,34 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	pusher.Events = change_decter.Events
-	go change_decter.SampleWorker()
+	setupServiceCenterAPIProxy(e)
+	setupWebSocket(e)
 
-	// WebSocket
-	e.GET("/websocket", pusher.Websocket)
 	// Routes
 	e.GET("/api/version", api.VersionGet)
 
 	// Start server
 	e.Logger.Info(e.Start(":3000"))
+}
+
+
+func setupWebSocket(e *echo.Echo) {
+	pusher.Events = change_decter.Events
+	go change_decter.SampleWorker()
+	e.GET("/websocket", pusher.Websocket)
+}
+
+func setupServiceCenterAPIProxy(e *echo.Echo) {
+	url1, err := url.Parse("http://127.0.0.1:30100")
+	if err != nil {
+		e.Logger.Fatal(err)
+	}
+	targets := []*middleware.ProxyTarget{
+		{
+			URL: url1,
+		},
+	}
+	g := e.Group("/v4")
+	g.Use(middleware.Proxy(middleware.NewRoundRobinBalancer(targets)))
+
 }
