1d81396228505c1e3b4c0215dc2140cc7ee93e61
xzccfzy <50447852+xzccfzy@users.noreply.github.com>
2020-11-10 15:01:37 +0800

    [SCB-2094] Implement mongo datasource interface (#737)

---
+++ datasource/mongo/account.go
@@ -0,0 +1,59 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package mongo
+
+import (
+	"context"
+	"github.com/apache/servicecomb-service-center/pkg/rbacframe"
+)
+
+func (ds *DataSource) AccountExist(ctx context.Context, key string) (bool, error) {
+	return false, nil
+}
+
+func (ds *DataSource) GetAccount(ctx context.Context, key string) (*rbacframe.Account, error) {
+	return &rbacframe.Account{}, nil
+}
+
+func (ds *DataSource) ListAccount(ctx context.Context, key string) ([]*rbacframe.Account, int64, error) {
+	return nil, 0, nil
+}
+
+func (ds *DataSource) DeleteAccount(ctx context.Context, key string) (bool, error) {
+	return false, nil
+}
+
+func (ds *DataSource) UpdateAccount(ctx context.Context, key string, account *rbacframe.Account) error {
+	return nil
+}
+
+func (ds *DataSource) AddDomain(ctx context.Context, domain string) (bool, error) {
+	return false, nil
+}
+
+func (ds *DataSource) DomainExist(ctx context.Context, domain string) (bool, error) {
+	return false, nil
+}
+
+func (ds *DataSource) AddProject(ctx context.Context, domain, project string) (bool, error) {
+	return false, nil
+}
+
+func (ds *DataSource) ProjectExist(ctx context.Context, domain, project string) (bool, error) {
+	return false, nil
+}
