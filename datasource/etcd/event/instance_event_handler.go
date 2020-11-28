094deff88fc7bdc57f55180d5792bacdf000c785
fuziye01 <74761148+fuziye01@users.noreply.github.com>
2020-11-28 11:28:04 +0800

    Incremental synchronization of sc: sync between sc and syncer (#755)
    * sc增量同步特性：sc与syncer之前的数据同步
    
    * 冲突解决，registry仓库换为go-chassis中的dump
    
    * 修改检视意见：
    1.onEvent里处理err
    2.增加配置项，对syncer的推送逻辑做到可插拔
    3.删除不需要的配置初始化流程
    
    * 配置文件app.yaml加上空行
    
    * set syncer.enable config default value false
    
    * 1. set syncer.enable in config to syncer.enabled
    2. set string "/cse-sr/ms/files/" to const
    3. add a switch to syncer websocket , only when syncer.enabled is true syncer can get websocket conn with sc
    
    * 1. 删除syncer websocket url中没有用到的project字段
    2. 注册的时候就判断syncer.enabled是否开启，如果没有开启就不注册syncer的接口
    3. 通过if中的条件语句简化嵌套
    
    * 1. if中的条件语句简化嵌套，通过抽象增加易读性
    
    * UT修改
    之前的顺序会先执行syncer的init方法初始化，然后执行用例
    初始化时候因为没有app.yaml配置文件config.GetBool()会出现pannic，导致用例无法执行，config也无法mock
    把service和service_test的单独放一个目录，然后根据包的导入顺序，先

--- datasource/etcd/event/instance_event_handler.go
+++ datasource/etcd/event/instance_event_handler.go
@@ -24,11 +24,13 @@ import (
 	"github.com/apache/servicecomb-service-center/datasource/etcd/path"
 	"github.com/apache/servicecomb-service-center/datasource/etcd/sd"
 	serviceUtil "github.com/apache/servicecomb-service-center/datasource/etcd/util"
+	"github.com/apache/servicecomb-service-center/pkg/dump"
 	"github.com/apache/servicecomb-service-center/pkg/log"
 	"github.com/apache/servicecomb-service-center/pkg/util"
 	"github.com/apache/servicecomb-service-center/server/core"
 	"github.com/apache/servicecomb-service-center/server/metrics"
 	"github.com/apache/servicecomb-service-center/server/notify"
+	"github.com/apache/servicecomb-service-center/server/syncernotify"
 	pb "github.com/go-chassis/cari/discovery"
 	"strings"
 )
@@ -36,6 +38,8 @@ import (
 const (
 	increaseOne = 1
 	decreaseOne = -1
+	msKeyPrefix = "/cse-sr/ms/files/"
+	sep         = "/"
 )
 
 // InstanceEventHandler is the handler to handle:
@@ -83,17 +87,22 @@ func (h *InstanceEventHandler) OnEvent(evt sd.KvEvent) {
 		}
 	}
 
-	if notify.Center().Closed() {
-		log.Warnf("caught [%s] instance[%s/%s] event, endpoints %v, but notify service is closed",
+	// 查询服务版本信息
+	ctx := util.WithGlobal(util.WithCacheOnly(context.Background()))
+	ms, err := serviceUtil.GetService(ctx, domainProject, providerID)
+
+	if ms == nil {
+		log.Errorf(err, "caught [%s] instance[%s/%s] event, endpoints %v, get cached provider's file failed",
 			action, providerID, providerInstanceID, instance.Endpoints)
 		return
 	}
 
-	// 查询服务版本信息
-	ctx := util.WithGlobal(util.WithCacheOnly(context.Background()))
-	ms, err := serviceUtil.GetService(ctx, domainProject, providerID)
-	if ms == nil {
-		log.Errorf(err, "caught [%s] instance[%s/%s] event, endpoints %v, get cached provider's file failed",
+	if !syncernotify.GetSyncerNotifyCenter().Closed() {
+		NotifySyncerInstanceEvent(evt, domainProject, ms)
+	}
+
+	if notify.Center().Closed() {
+		log.Warnf("caught [%s] instance[%s/%s] event, endpoints %v, but notify service is closed",
 			action, providerID, providerInstanceID, instance.Endpoints)
 		return
 	}
@@ -141,4 +150,27 @@ func PublishInstanceEvent(evt sd.KvEvent, domainProject string, serviceKey *pb.M
 			log.Errorf(err, "publish event[%v] into channel failed", evt)
 		}
 	}
+}
+
+func NotifySyncerInstanceEvent(evt sd.KvEvent, domainProject string, ms *pb.MicroService) {
+	msInstance := evt.KV.Value.(*pb.MicroServiceInstance)
+
+	serviceKey := msKeyPrefix + domainProject + sep + ms.ServiceId
+	msKV := &dump.KV{Key: serviceKey, ClusterName: evt.KV.ClusterName}
+	service := &dump.Microservice{KV: msKV, Value: ms}
+
+	instKey := string(evt.KV.Key)
+	instKV := &dump.KV{Key: instKey, ClusterName: evt.KV.ClusterName}
+	instance := &dump.Instance{KV: instKV, Value: msInstance}
+
+	instEvent := &dump.WatchInstanceChangedEvent{
+		Action:   string(evt.Type),
+		Service:  service,
+		Instance: instance,
+		Revision: evt.Revision,
+	}
+
+	syncernotify.GetSyncerNotifyCenter().AddEvent(instEvent)
+
+	log.Debugf("success to add instance change event:%s to event queue", instEvent)
 }
