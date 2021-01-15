43504806e2de260886093dd09fcc2d2527b364e2
panqian <pqa1996826@163.com>
2021-01-15 16:03:05 +0800

    [SCB-2094] support instance subscribe (#827)

---
+++ datasource/mongo/rule_util_test.go
@@ -0,0 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package mongo_test
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/apache/servicecomb-service-center/datasource"
+	"github.com/apache/servicecomb-service-center/datasource/mongo"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRuleFilter_Filter(t *testing.T) {
+	var err error
+	t.Run("when there is no such a customer in db", func(t *testing.T) {
+		_, err = mongo.Filter(context.Background(), []*mongo.Rule{}, "")
+		if err != nil && !errors.Is(err, datasource.ErrNoData) {
+			t.Fatalf("RuleFilter Filter failed")
+		}
+		assert.Equal(t, datasource.ErrNoData, err, "no data found")
+	})
+	t.Run("FilterAll when customer not exist", func(t *testing.T) {
+		_, _, err = mongo.FilterAll(context.Background(), []string{""}, []*mongo.Rule{})
+		if err != nil && !errors.Is(err, datasource.ErrNoData) {
+			t.Fatalf("RuleFilter FilterAll failed")
+		}
+		assert.Equal(t, nil, err, "no customer found err is nil")
+	})
+	t.Run("FilterAll when ProviderRules not nil and service not exist", func(t *testing.T) {
+		_, _, err = mongo.FilterAll(context.Background(), []string{""}, []*mongo.Rule{{}})
+		if err != nil && !errors.Is(err, datasource.ErrNoData) {
+			t.Fatalf("RuleFilter FilterAll failed")
+		}
+		assert.Equal(t, datasource.ErrNoData, err, "no customer found when FilterAll")
+	})
+}
