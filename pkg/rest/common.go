e640a6f5daf51ddf8fdf2fc77c684289515236cc
little-cui <sure_0757@qq.com>
2021-05-21 09:17:18 +0800

    SCB-2176 Refactor rest router (#994)

--- pkg/rest/common.go
+++ pkg/rest/common.go
@@ -64,3 +64,31 @@ func isValidMethod(method string) bool {
 		return false
 	}
 }
+
+func match(s string, f func(c byte) bool, exclude byte, i int) (matched string, next byte, j int) {
+	j = i
+	for j < len(s) && f(s[j]) && s[j] != exclude {
+		j++
+	}
+
+	if j < len(s) {
+		next = s[j]
+	}
+	return s[i:j], next, j
+}
+
+func matchParticipial(c byte) bool {
+	return c != '/'
+}
+
+func isAlpha(ch byte) bool {
+	return ('a' <= ch && ch <= 'z') || ('A' <= ch && ch <= 'Z') || ch == '_'
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+
+func isAlnum(ch byte) bool {
+	return isAlpha(ch) || isDigit(ch)
+}
---
+++ pkg/rest/rest.go
@@ -0,0 +1,40 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package rest
+
+import (
+	"net/http"
+)
+
+var router *Router // router is the default handler
+
+func init() {
+	router = NewRouter()
+	router.setChainName(ServerChainName)
+}
+
+// RegisterServant registers a RouteGroup into router
+// servant must be an pointer to service object
+func RegisterServant(group RouteGroup) {
+	router.RegisterServant(group)
+}
+
+//GetRouter return the router fo REST service
+func GetRouter() http.Handler {
+	return router
+}
