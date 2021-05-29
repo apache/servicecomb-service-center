d9b8ba8fea32d852bdb5a79b04cb82e654169660
little-cui <sure_0757@qq.com>
2021-05-29 19:08:12 +0800

    SCB-2176 Fix: targetResource label key not exist should be pass (#1019)
    * SCB-2176 Fix: targetResource label key not exist should be pass
    
    * SCB-2176 Refactor parse UTs
    
    * SCB-2176 Resolve comments

--- server/plugin/auth/buildin/parser_test.go
+++ server/plugin/auth/buildin/parser_test.go
@@ -18,20 +18,26 @@
 package buildin_test
 
 import (
+	_ "github.com/apache/servicecomb-service-center/test"
+
 	"context"
 	"net/http"
 	"strings"
 	"testing"
 
-	_ "github.com/apache/servicecomb-service-center/test"
-
 	"github.com/apache/servicecomb-service-center/datasource"
+	"github.com/apache/servicecomb-service-center/pkg/rest"
+	"github.com/apache/servicecomb-service-center/pkg/util"
+	"github.com/apache/servicecomb-service-center/server/plugin/auth"
 	"github.com/apache/servicecomb-service-center/server/plugin/auth/buildin"
+	"github.com/apache/servicecomb-service-center/server/service/rbac"
 	"github.com/go-chassis/cari/discovery"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestGetAPIParseFunc(t *testing.T) {
+	rbac.InitResourceMap()
+
 	var serviceIDA, serviceIDB string
 
 	response, _ := datasource.Instance().RegisterService(context.Background(), &discovery.CreateServiceRequest{
@@ -49,85 +55,235 @@ func TestGetAPIParseFunc(t *testing.T) {
 	})
 	serviceIDB = response.ServiceId
 
-	t.Run("get all services api should return no labels", func(t *testing.T) {
-		request, err := http.NewRequest(http.MethodGet, "/v4/default/registry/microservices", nil)
-		assert.NoError(t, err)
+	newRequest := func(method, url string, body string) *http.Request {
+		request, _ := http.NewRequest(method, url, strings.NewReader(body))
+		util.SetRequestContext(request, rest.CtxMatchPattern, url)
+		return request
+	}
+	tests := []struct {
+		name         string
+		request      *http.Request
+		wantResource *auth.ResourceScope
+		wantErr      bool
+	}{
+		{
+			"get all services api should return no labels",
+			newRequest(http.MethodGet, "/v4/:project/registry/microservices", ""),
+			&auth.ResourceScope{
+				Type: "service",
+				Verb: "get",
+			},
+			false,
+		},
+		{
+			"create services api without body should return err",
+			newRequest(http.MethodPost, "/v4/:project/registry/microservices", "{}"),
+			nil,
+			true,
+		},
+		{
+			"create services api with serviceName A should return A",
+			newRequest(http.MethodPost, "/v4/:project/registry/microservices",
+				"{\n  \"service\": {\n    \"serviceName\": \"A\"\n  }\n}"),
+			&auth.ResourceScope{
+				Type: "service",
+				Labels: []map[string]string{
+					{
+						"environment": "",
+						"appId":       "",
+						"serviceName": "A",
+					},
+				},
+				Verb: "create",
+			},
+			false,
+		},
+		{
+			"delete 2 services api should return 2 labels",
+			newRequest(http.MethodDelete, "/v4/:project/registry/microservices",
+				"{\n  \"serviceIds\": [\""+serviceIDA+"\", \""+serviceIDB+"\"]\n}"),
+			&auth.ResourceScope{
+				Type: "service",
+				Labels: []map[string]string{
+					{
+						"environment": "",
+						"appId":       "TestGetAPIParseFunc",
+						"serviceName": "A",
+					},
+					{
+						"environment": "",
+						"appId":       "TestGetAPIParseFunc",
+						"serviceName": "B",
+					},
+				},
+				Verb: "delete",
+			},
+			false,
+		},
+		{
+			"get service sub resource api should return service labels",
+			newRequest(http.MethodGet, "/v4/:project/registry/microservices/:serviceId/instances?:serviceId="+serviceIDA, ""),
+			&auth.ResourceScope{
+				Type: "service",
+				Labels: []map[string]string{
+					{
+						"environment": "",
+						"appId":       "TestGetAPIParseFunc",
+						"serviceName": "A",
+					},
+				},
+				Verb: "get",
+			},
+			false,
+		},
+		{
+			"discovery A instances api should return service A labels",
+			newRequest(http.MethodGet, "/v4/:project/registry/instances?serviceName=A", ""),
+			&auth.ResourceScope{
+				Type: "service",
+				Labels: []map[string]string{
+					{
+						"environment": "",
+						"appId":       "",
+						"serviceName": "A",
+					},
+				},
+				Verb: "get",
+			},
+			false,
+		},
+		{
+			"govern query A api should return service A labels",
+			newRequest(http.MethodGet, "/v4/:project/govern/microservices?serviceName=A", ""),
+			&auth.ResourceScope{
+				Type: "service",
+				Verb: "get",
+			},
+			false,
+		},
+		{
+			"govern query all api should return nil labels",
+			newRequest(http.MethodGet, "/v4/:project/govern/microservices", ""),
+			&auth.ResourceScope{
+				Type: "service",
+				Verb: "get",
+			},
+			false,
+		},
+		{
+			"batch discovery A instances should return A labels",
+			newRequest(http.MethodPost, "/v4/:project/registry/instances/action",
+				"{\n  \"consumerId\": \"\",\n  \"instances\": [\n    {\n      \"instance\": {\n        \"serviceId\": \""+serviceIDA+"\",\n        \"instanceId\": \"\"\n      },\n      \"rev\": \"\"\n    }\n  ]\n}"),
+			&auth.ResourceScope{
+				Type: "service",
+				Labels: []map[string]string{
+					{
+						"environment": "",
+						"appId":       "TestGetAPIParseFunc",
+						"serviceName": "A",
+					},
+				},
+				Verb: "get",
+			},
+			false,
+		},
+		{
+			"batch heartbeat A instances should return A labels",
+			newRequest(http.MethodPost, "/v4/:project/registry/heartbeats",
+				"{\n  \"instances\": [\n    {\n      \"serviceId\": \""+serviceIDA+"\",\n      \"instanceId\": \"\"\n    }\n  ]\n}"),
+			&auth.ResourceScope{
+				Type: "service",
+				Labels: []map[string]string{
+					{
+						"environment": "",
+						"appId":       "TestGetAPIParseFunc",
+						"serviceName": "A",
+					},
+				},
+				Verb: "update",
+			},
+			false,
+		},
+		// not registered api
+		{
+			"govern statistics api should apply all",
+			newRequest(http.MethodGet, "/v4/:project/govern/statistics", ""),
+			&auth.ResourceScope{
+				Type: "service",
+				Verb: "get",
+			},
+			false,
+		},
+		{
+			"govern apps api should apply all",
+			newRequest(http.MethodGet, "/v4/:project/govern/apps", ""),
+			&auth.ResourceScope{
+				Type: "service",
+				Verb: "get",
+			},
+			false,
+		},
+		{
+			"account api should apply all",
+			newRequest(http.MethodGet, "/v4/accounts", ""),
+			&auth.ResourceScope{
+				Type: "account",
+				Verb: "get",
+			},
+			false,
+		},
+		{
+			"role api should apply all",
+			newRequest(http.MethodGet, "/v4/roles", ""),
+			&auth.ResourceScope{
+				Type: "role",
+				Verb: "get",
+			},
+			false,
+		},
+		{
+			"role api should apply all",
+			newRequest(http.MethodGet, "/v1/:project/gov/", ""),
+			&auth.ResourceScope{
+				Type: "governance",
+				Verb: "get",
+			},
+			false,
+		},
+	}
 
-		labels, err := buildin.GetAPIParseFunc("/v4/:project/registry/microservices")(request)
-		assert.NoError(t, err)
-		assert.Nil(t, labels)
-	})
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			resource, err := buildin.GetAPIParseFunc(tt.request.URL.Path)(tt.request)
+			assert.Equal(t, tt.wantErr, err != nil)
+			assert.Equal(t, tt.wantResource == nil, resource == nil)
+			if tt.wantResource == nil {
+				return
+			}
+			assert.Equal(t, tt.wantResource.Type, resource.Type)
+			assert.Equal(t, tt.wantResource.Verb, resource.Verb)
+			assert.Equal(t, tt.wantResource.Labels == nil, resource.Labels == nil)
+			for _, labels := range tt.wantResource.Labels {
+				checkLabels(t, labels, resource)
+			}
+		})
+	}
+}
 
-	t.Run("create services api without body should return err", func(t *testing.T) {
-		reader := strings.NewReader("{}")
-
-		request, err := http.NewRequest(http.MethodPost, "/v4/default/registry/microservices", reader)
-		assert.NoError(t, err)
-
-		_, err = buildin.GetAPIParseFunc("/v4/:project/registry/microservices")(request)
-		assert.Error(t, err)
-	})
-
-	t.Run("create services api with serviceName A should return A", func(t *testing.T) {
-		reader := strings.NewReader("{\n  \"service\": {\n    \"serviceName\": \"A\"\n  }\n}")
-
-		request, err := http.NewRequest(http.MethodPost, "/v4/default/registry/microservices", reader)
-		assert.NoError(t, err)
-
-		labels, err := buildin.GetAPIParseFunc("/v4/:project/registry/microservices")(request)
-		assert.NoError(t, err)
-		assert.Equal(t, 1, len(labels))
-		assert.Equal(t, "A", labels[0]["serviceName"])
-	})
-
-	t.Run("delete 2 services api should return 2 labels", func(t *testing.T) {
-		reader := strings.NewReader("{\n  \"serviceIds\": [\"" + serviceIDA + "\", \"" + serviceIDB + "\"]\n}")
-
-		request, err := http.NewRequest(http.MethodDelete, "/v4/default/registry/microservices", reader)
-		assert.NoError(t, err)
-
-		labels, err := buildin.GetAPIParseFunc("/v4/:project/registry/microservices")(request)
-		assert.NoError(t, err)
-		assert.Equal(t, 2, len(labels))
-		assert.Equal(t, "A", labels[0]["serviceName"])
-	})
-
-	t.Run("get service sub resource api should return service labels", func(t *testing.T) {
-		request, err := http.NewRequest(http.MethodGet, "/v4/default/registry/microservices/"+serviceIDA+"/instances?:serviceId="+serviceIDA, nil)
-		assert.NoError(t, err)
-
-		labels, err := buildin.GetAPIParseFunc("/v4/:project/registry/microservices/:serviceId/instances")(request)
-		assert.NoError(t, err)
-		assert.Equal(t, 1, len(labels))
-		assert.Equal(t, "A", labels[0]["serviceName"])
-	})
-
-	t.Run("discovery A instances api should return service A labels", func(t *testing.T) {
-		request, err := http.NewRequest(http.MethodGet, "/v4/default/registry/instances?serviceName=A", nil)
-		assert.NoError(t, err)
-
-		labels, err := buildin.GetAPIParseFunc("/v4/:project/registry/instances")(request)
-		assert.NoError(t, err)
-		assert.Equal(t, 1, len(labels))
-		assert.Equal(t, "A", labels[0]["serviceName"])
-	})
-
-	t.Run("govern query A api should return service A labels", func(t *testing.T) {
-		request, err := http.NewRequest(http.MethodGet, "/v4/default/govern/microservices?serviceName=A", nil)
-		assert.NoError(t, err)
-
-		labels, err := buildin.GetAPIParseFunc("/v4/:project/govern/microservices")(request)
-		assert.NoError(t, err)
-		assert.Equal(t, 1, len(labels))
-		assert.Equal(t, "A", labels[0]["serviceName"])
-	})
-
-	t.Run("govern query all api should return nil labels", func(t *testing.T) {
-		request, err := http.NewRequest(http.MethodGet, "/v4/default/govern/microservices", nil)
-		assert.NoError(t, err)
-
-		labels, err := buildin.GetAPIParseFunc("/v4/:project/govern/microservices")(request)
-		assert.NoError(t, err)
-		assert.Nil(t, labels)
-	})
+func checkLabels(t *testing.T, labels map[string]string, resource *auth.ResourceScope) {
+	var (
+		targetLabelsLength int
+		found              bool
+	)
+	for k, v := range labels {
+		for _, targetLabels := range resource.Labels {
+			targetLabelsLength = len(targetLabels)
+			found = v == targetLabels[k]
+			if found {
+				break
+			}
+		}
+		assert.True(t, found, "target label key value not matched %s=%s", k, v)
+	}
+	assert.Equal(t, len(labels), targetLabelsLength)
 }
