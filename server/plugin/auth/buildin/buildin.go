c29f5a08b9040fbeabe12737f73290ca89a30448
SphaIris <aaalixiaopei123>
2021-08-04 22:53:53 +0800

    set token

--- server/plugin/auth/buildin/buildin.go
+++ server/plugin/auth/buildin/buildin.go
@@ -155,7 +155,7 @@ func (ba *TokenAuthenticator) VerifyToken(req *http.Request) (interface{}, error
 	if err != nil {
 		return nil, err
 	}
-	token.WithRequest(req, to)
+	token.WithRequest(req, v)
 	return claims, nil
 }
 
