4efb9bf1461e8d987c77be9492894e3bbe0f6ac3
robotljw <790504160@qq.com>
2021-11-29 22:05:40 +0800

    [feat] add eventbase      1.add datasource in eventbase

---
+++ eventbase/datasource/options.go
@@ -0,0 +1,68 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package datasource
+
+type TaskFindOptions struct {
+	Action   string
+	Status   string
+	DataType string
+}
+
+type TombstoneFindOptions struct {
+	ResourceType    string
+	BeforeTimestamp int64
+}
+
+type TaskFindOption func(options *TaskFindOptions)
+
+type TombstoneFindOption func(options *TombstoneFindOptions)
+
+// WithAction find task with action
+func WithAction(action string) TaskFindOption {
+	return func(options *TaskFindOptions) {
+		options.Action = action
+	}
+}
+
+// WithStatus find task with status
+func WithStatus(status string) TaskFindOption {
+	return func(options *TaskFindOptions) {
+		options.Status = status
+	}
+}
+
+// WithDataType find task with dataType
+func WithDataType(dataType string) TaskFindOption {
+	return func(options *TaskFindOptions) {
+		options.DataType = dataType
+	}
+}
+
+// WithResourceType find tombstone with resource type
+func WithResourceType(resourceType string) TombstoneFindOption {
+	return func(options *TombstoneFindOptions) {
+		options.ResourceType = resourceType
+	}
+}
+
+// WithBeforeTimestamp find tombstone with beforeTimestamp
+func WithBeforeTimestamp(timestamp int64) TombstoneFindOption {
+	return func(options *TombstoneFindOptions) {
+		options.BeforeTimestamp = timestamp
+	}
+}
